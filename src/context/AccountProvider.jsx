66143eb6438fe73397207cd8f7b8bee4c015a692
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2024-02-13 15:44:09 +0530

    Add files via upload

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -16,7 +16,7 @@ const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(() => {
-        socket.current = io('ws://localhost:9000');
+        socket.current = io('https://periwinkle-concise-olivine.glitch.me/');
     }, [])
 
     return (

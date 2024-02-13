22c8ad9d54eec48c16882c40df5f07d0744e43d6
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2024-02-13 16:28:35 +0530

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,4 +1,4 @@
-export const clientId = '1051426514050-dv0o9fu9umand32ov9iqcamvuf9gttta.apps.googleusercontent.com'
+export const clientId = '1015651992509-j3fkpg2l6eu462nda29rhgn1klfu006o.apps.googleusercontent.com'
 
 export const iconPDF = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/27_Pdf_File_Type_Adobe_logo_logos-512.png';
 export const defaultProfilePicture = 'https://static.straitstimes.com.sg/s3fs-public/articles/2020/12/01/af_moneyheist_011220.jpg';
